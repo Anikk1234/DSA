e9ca72dadcbc2a516492497771042500356838df
Anik Kirtania <147408948+Anikk1234@users.noreply.github.com>
2025-02-15 13:34:30 +0600

    Update sqrtRootDecomposition.cpp

--- sqrtRootDecomposition.cpp
+++ sqrtRootDecomposition.cpp
@@ -1,82 +1,60 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-const int MAX_N = 30000;
-const int MAX_A = 1000000;
-const int MAX_Q = 200000;
+class SqrtDecomposition {
+    vector<int> arr, block;
+    int blockSize, n;
 
-struct Query {
-    int L, R, index;
+public:
+    // Constructor: Initialize data structures
+    SqrtDecomposition(vector<int>& input) {
+        n = input.size();
+        blockSize = sqrt(n);
+        arr = input;
+        block.resize((n + blockSize - 1) / blockSize, 0);
+
+        // Precompute block sums
+        for (int i = 0; i < n; i++)
+            block[i / blockSize] += arr[i];
+    }
+
+    // Range sum query
+    int query(int left, int right) {
+        int sum = 0;
+
+        while (left <= right) {
+            // If within a full block, add the whole block sum
+            if (left % blockSize == 0 && left + blockSize - 1 <= right) {
+                sum += block[left / blockSize];
+                left += blockSize;
+            }
+            // Otherwise, add individual elements
+            else {
+                sum += arr[left];
+                left++;
+            }
+        }
+
+        return sum;
+    }
+
+    // Update an element
+    void update(int index, int value) {
+        int blockIndex = index / blockSize;
+        block[blockIndex] += value - arr[index];  // Update block sum
+        arr[index] = value;  // Update actual array
+    }
 };
 
-// Mo's Algorithm Comparator
-int blockSize;
-bool compare(Query a, Query b) {
-    if (a.L / blockSize != b.L / blockSize)
-        return a.L / blockSize < b.L / blockSize;
-    return (a.L / blockSize & 1) ? (a.R < b.R) : (a.R > b.R);
-}
-
-vector<int> moAlgorithm(vector<int>& arr, vector<Query>& queries) {
-    int n = arr.size();
-    int q = queries.size();
-    vector<int> answers(q);
-    vector<int> freq(MAX_A + 1, 0);
-    
-    blockSize = sqrt(n);
-    sort(queries.begin(), queries.end(), compare);
-
-    int currL = 0, currR = -1, distinctCount = 0;
-
-    // Add function
-    auto add = [&](int idx) {
-        if (freq[arr[idx]] == 0) distinctCount++;
-        freq[arr[idx]]++;
-    };
-
-    // Remove function
-    auto remove = [&](int idx) {
-        freq[arr[idx]]--;
-        if (freq[arr[idx]] == 0) distinctCount--;
-    };
-
-    for (Query q : queries) {
-        while (currL > q.L) add(--currL);
-        while (currR < q.R) add(++currR);
-        while (currL < q.L) remove(currL++);
-        while (currR > q.R) remove(currR--);
-
-        answers[q.index] = distinctCount;
-    }
-
-    return answers;
-}
-
+// Driver function
 int main() {
-    int n, q;
-    cin >> n;
-    vector<int> arr(n);
+    vector<int> input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    SqrtDecomposition sqrtDecomp(input);
 
-    for (int i = 0; i < n; i++) cin >> arr[i];
-
-    cin >> q;
-    vector<Query> queries(q);
-
-    for (int i = 0; i < q; i++) {
-        cin >> queries[i].L >> queries[i].R;
-        queries[i].L--; // Convert to 0-based indexing
-        queries[i].R--;
-        queries[i].index = i; // Store query index
-    }
-
-    vector<int> results = moAlgorithm(arr, queries);
-
-    for (int res : results)
-        cout << res << endl;
+    cout << "Sum of range [1, 7]: " << sqrtDecomp.query(1, 7) << endl;  // Output: 35
+    sqrtDecomp.update(3, 10);  // Update arr[3] = 10
+    cout << "Sum of range [1, 7] after update: " << sqrtDecomp.query(1, 7) << endl;  // Output: 41
 
     return 0;
 }
